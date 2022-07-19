c8a86431f940295c00bb37d853034d8615dec523
EmotoyBunny <sonka.zaichkina@mail.ru>
2022-07-19 20:28:58 +0300

    доработки

--- src/components/MapStart.jsx
+++ src/components/MapStart.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {MapContainer, Polygon, Popup, TileLayer, Marker, FeatureGroup, Circle, useMapEvents} from 'react-leaflet';
+import {MapContainer, Polygon, Popup, TileLayer, Marker, FeatureGroup, Circle, useMapEvent} from 'react-leaflet';
 import {divIcon} from "leaflet";
 import {EditControl} from "react-leaflet-draw"
 import {renderToStaticMarkup} from "react-dom/server";
@@ -18,39 +18,28 @@ class MapStart extends Component {
                 center: [47.5690, 38.8697],
                 zoom: 9
             },
-            markers: [[47.21744415, 38.898958906],
+            markers: [
+                [47.21744415, 38.898958906],
                 [47.968478649, 37.769425424],
                 [47.2213858, 39.7114196],
                 [47.1179735, 37.6342808],
                 [47.41066, 40.101986],
                 [47.568459, 38.861942],
             ],
-            polygon: [[47.968478649, 37.76942542],
-                [47.1179735, 37.6342808],
-                [47.568459, 38.861942],
+            polygon: [
+                [
+                    [47.968478649, 37.76942542],
+                    [47.1179735, 37.6342808],
+                    [47.568459, 38.861942],
+                ]
             ],
             newPolygon: [],
-            markerSelect: [1, 1],
+            markerSelect: [47.21744415, 38.898958906], // координаты Таганрога
         };
         this.mapRef = React.createRef();
         this.markerRef = React.createRef();
+        this.polygonRef = React.createRef();
     }
-
-    /*  componentDidUpdate = (prevState) => {
-           if (this.state.markerSelect !== [] && (this.state.markerSelect !== prevState.markerSelect)) {
-               console.log(1)
-               this.mapRef?.current?.leafletElement.flyTo(this.state.markerSelect, 12);
-           }
-       };*/
-
-    handleClickMarker = () => {
-        this.setState({markerSelect: [47.21744415, 38.898958906]});
-       /* const map = useMapEvents(
-            () => {
-                map.flyTo([47.21744415, 38.898958906], 12)
-            },
-        )*/
-    };
 
     render() {
         const {mapOptions, markers, polygon} = this.state;
@@ -63,51 +52,56 @@ class MapStart extends Component {
                     height: "100vh",
                     width: "100vw"
                 }}
+
                 center={mapOptions.center}
                 ref={this.mapRef}
             >
-
                 <TileLayer
                     url={layer}
                 />
-                {markers.map((item, index) => {
-                    return (<Marker
-                        position={item}
-                        key={index}
-                        ref={this.markerRef}
-                        icon={divIcon({
-                            className: '',
-                            iconAnchor: [18, 31],
-                            html: renderToStaticMarkup(<RoomIcon
-                                style={{
-                                    color: iconColor[index],
-                                    width: "36px",
-                                    height: "36px"
-                                }}
-                            />),
-                        })}
-                        title={index}
-                        onClick={() => {
-                            console.log(1)
-                            //this.handleClickMarker()
-                        }}
-                    >
-                        <Popup>
-                            {(index + 1) + " [" + item[0] + ", " + item[1] + "]"}
-                        </Popup>
-                    </Marker>)
-                })}
-                <Polygon color='purple' positions={polygon}>
-                    <Popup>
-                        Какой-то полигон
-                    </Popup>
-                </Polygon>
-                <Circle
-                    center={[47.2213858, 39.7114196]}
-                    pathOptions={{fillColor: "red"}}
-                    radius={10000}
-                />
                 <FeatureGroup>
+                    <Circle
+                        center={[47.2213858, 39.7114196]}
+                        pathOptions={{fillColor: "red"}}
+                        radius={10000}
+                    />
+                    {markers.map((item, index) => {
+                        return (<Marker
+                            position={item}
+                            key={index}
+                            ref={this.markerRef}
+                            icon={divIcon({
+                                className: '',
+                                iconAnchor: [18, 31],
+                                html: renderToStaticMarkup(<RoomIcon
+                                    style={{
+                                        color: iconColor[index],
+                                        width: "36px",
+                                        height: "36px"
+                                    }}
+                                />),
+                            })}
+                            title={index}
+                            eventHandlers={{
+                                click: () => {
+                                    this.mapRef.current.setView(this.state.markerSelect, 12)
+                                }
+                            }}
+
+                        >
+                            <Popup>
+                                {(index + 1) + " [" + item[0] + ", " + item[1] + "]"}
+                            </Popup>
+                        </Marker>)
+                    })}
+                    {polygon.map((item, index) => {
+                        return (
+                            <Polygon color='purple' key={index} ref={this.polygonRef} positions={item}>
+                                <Popup>
+                                    {"Какой-то полигон №" + index}
+                                </Popup>
+                            </Polygon>)
+                    })}
                     <EditControl
                         position='topleft'
                         onEdited={() => {
@@ -116,11 +110,13 @@ class MapStart extends Component {
                             console.log(e);
                             if (e.layerType === 'polygon') {
                                 let objectsLayer = e.layer._rings;
+                                let array = polygon;
                                 let arrayLayer = [];
                                 for (let i = 0; i < objectsLayer[0].length; i++) {
                                     arrayLayer.push([objectsLayer[0][i].x, objectsLayer[0][i].y])
                                 }
-                                this.setState({newPolygon: arrayLayer}, () => {
+                                array.push(arrayLayer);
+                                this.setState({newPolygon: arrayLayer, polygon: array}, () => {
                                     console.log(this.state.newPolygon);
                                     let text = "";
                                     for (let i = 0; i < this.state.newPolygon.length; i++) {
